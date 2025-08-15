a1e49d0f1bdb9e30f7c6e6a697e383f1dcace3df
Rohan Prajapati <rohanprajapati1503@gmail.com>
2025-08-16 02:09:12 +0530

    Add_sound

---
+++ engine/features.py
@@ -0,0 +1,6 @@
+from playsound import playsound
+
+def playAssistentSound():
+    music_dir="www\\assets\\audio\\start_sound.mp3"
+    playsound(music_dir)
+
---
+++ www/main.js
@@ -0,0 +1,29 @@
+ var siriWave = new SiriWave({
+    container: document.getElementById("siri-container"),
+    width: 900,
+    height: 200,
+    style:"ios9",
+    amplitude:'1',
+    speed:"0.30",
+    autostart:true
+  });
+
+const message = document.querySelector(".siri-message");
+
+function animateText() {
+  // Fade In
+  message.classList.add("fadeInUp");
+  message.classList.remove("fadeOutUp");
+
+  // Wait 3 seconds, then fade out
+  setTimeout(() => {
+    message.classList.remove("fadeInUp");
+    message.classList.add("fadeOutUp");
+  }, 3000);
+}
+
+// Start animation every 5 seconds
+setInterval(animateText, 5000);
+
+// Run the first time immediately
+animateText();
